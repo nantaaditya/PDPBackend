914a6ef71b521d721d8537ac8b9a1b79b45831cd
Pramuditya Anantanur <pramuditza@gmail.com>
2017-03-27 20:54:48 +0700

    Initial commit

---
+++ src/main/java/blibli/ptp/BlibliPtpApplication.java
@@ -0,0 +1,12 @@
+package blibli.ptp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlibliPtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlibliPtpApplication.class, args);
+	}
+}
